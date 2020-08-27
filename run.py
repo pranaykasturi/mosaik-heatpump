5bd0a1375ac42400d2cb1728486e91f56b16fdc5
Pranay Kasturi <pranaykasturi@gmail.com>
2020-08-28 00:21:14 +0200

    Using mosaik-csv instead of csvschedulesim

--- run.py
+++ run.py
@@ -8,20 +8,24 @@ SIM_CONFIG = {
         'python': 'Heat_Pump_mosaik:HeatPumpSimulator',
     },
     'CSV': {
-        'python': 'csvschedule_mosaik:CSVScheduleSimulator',
+        'python': 'mosaik_csv:CSV',
     },
     'DB': {
         'cmd': 'mosaik-hdf5 %(addr)s'
     },
 }
-END = 50 * 15 * 60  # 12.5 Hours or 750 mins
+
+START = '2016-01-01 00:00:00'
+END = 10 * 15 * 60  # 2.5 Hours or 150 mins
+HEAT_LOAD_DATA = 'Heatload3.csv'
 
 # Create World
 world = mosaik.World(SIM_CONFIG)
 
 # Start simulators
 heatpumpsim = world.start('HeatPumpSim', step_size=15*60)
-csv = world.start('CSV', step_size=15*60)
+csv = world.start('CSV', sim_start=START, datafile=HEAT_LOAD_DATA)
+# csv = world.start('CSV', step_size=15*60)
 
 params = {'cd_cons_temp': 90,
           'cb_dhp_temp': 60,
@@ -33,18 +37,16 @@ params = {'cd_cons_temp': 90,
           'ic_out_temp': 30
           }
 
-params1 = {'filename': 'HeatLoad1.csv'}
-
 # Instantiate models
 heatpump = heatpumpsim.HeatPump(params=params)
-csvfile = csv.CSVSchedule(params=params1)
+heat_load = csv.HP.create(1)[0]
 
 # Connect entities
-world.connect(csvfile, heatpump, 'cons_Q')
+world.connect(heat_load, heatpump, 'cons_Q')
 
 # Initializing and instantiating a database component:
 db = world.start('DB', step_size=15*60, duration=END)
-hdf5 = db.Database(filename='heat_pump_trial.hdf5')
+hdf5 = db.Database(filename='heat_pump_trial_mosaikcsv.hdf5')
 
 world.connect(heatpump, hdf5, 'cons_Q', 'amb_T', 'p_kw', 'COP')
 
