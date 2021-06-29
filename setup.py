c6761482ca42a05b48a1d94ed6b511d5632a65aa
pranaykasturi <pranaykas@gmail.com>
2021-06-29 15:04:56 +0530

    Added files for the build process

---
+++ setup.py
@@ -0,0 +1,48 @@
+from setuptools import setup, find_packages
+import os
+
+find_version='0.1.0'
+if os.environ.get('CI_COMMIT_TAG'):
+    find_version = os.environ['CI_COMMIT_TAG']
+
+
+
+setup(
+    name='mosaik-heatpump',
+    version=find_version,
+    author='Pranay Kasturi',
+    author_email='mosaik@offis.de',
+    description='Mosaik-heatpump provides a model of a residential heatpump system',
+    long_description=(open('README.rst', encoding='utf-8').read() + '\n\n' +
+                      open('CHANGES.txt', encoding='utf-8').read() + '\n\n' +
+                      open('AUTHORS.txt', encoding='utf-8').read()),
+    url='https://mosaik.offis.de',
+    install_requires=[
+        'networkx>=2.5',
+        'mosaik-api>=3.0',
+        'tespy>=0.4.2',
+    ],
+
+    packages=find_packages(exclude=['tests*']),
+    py_modules=['mosaik_heatpump'],
+    include_package_data=True,
+    entry_points={
+        'console_scripts': [
+        ],
+    },
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Console',
+        'Intended Audience :: Science/Research',
+        'Natural Language :: English',
+        'License :: OSI Approved :: GNU Lesser General Public License v2 (LGPLv2)',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Topic :: Scientific/Engineering',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+)
