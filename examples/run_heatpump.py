66a4f9f3cd2b3d80f4cbe56494b34d6c2a255391
pranaykasturi <pranaykas@gmail.com>
2022-01-17 09:54:26 +0100

    updated the example to work with the latest version of heatpump model

--- examples/run_heatpump.py
+++ examples/run_heatpump.py
@@ -1,5 +1,8 @@
 import mosaik.util
-
+import sys
+import os
+os.chdir(os.path.dirname(os.path.abspath(__file__)))
+sys.path.append(os.path.dirname(os.path.dirname(__file__)))
 
 # Sim config. and other parameters
 SIM_CONFIG = {
@@ -7,7 +10,7 @@ SIM_CONFIG = {
         'python': 'mosaik_heatpump.heatpump.Heat_Pump_mosaik:HeatPumpSimulator',
     },
     'CSV': {
-        'python': 'mosaik_csv_3:CSV',
+        'python': 'mosaik_csv:CSV',
     },
     'DB': {
         'cmd': 'mosaik-hdf5 %(addr)s'
@@ -16,7 +19,7 @@ SIM_CONFIG = {
 
 START = '01.01.2016 00:00'
 END = 10 * 15 * 60  # 2.5 Hours or 150 mins
-HEAT_LOAD_DATA = 'Heatload_R1.csv'
+HEAT_LOAD_DATA = 'data/Heatload_R1.csv'
 date_format = 'DD.MM.YYYY HH:mm'
 
 # Create World
@@ -27,10 +30,12 @@ heatpumpsim = world.start('HeatPumpSim', step_size=15*60)
 csv = world.start('CSV', sim_start=START, datafile=HEAT_LOAD_DATA, date_format=date_format)
 # csv = world.start('CSV', step_size=15*60)
 
-params = {'heat_source': 'air',
+params = {'hp_model': 'Air_8kW',
+          'heat_source': 'air',
           'cons_T': 35,
           'heat_source_T': 2,
-          'calc_mode': 'detailed',
+          'T_amb': 2,
+          'calc_mode': 'fast',
           }
 
 # Instantiate models
@@ -38,7 +43,8 @@ heatpump = heatpumpsim.HeatPump(params=params)
 heat_load = csv.HP()
 
 # Connect entities
-world.connect(heat_load, heatpump, 'Q_Demand', 'heat_source_T')
+world.connect(heat_load, heatpump, ('Q_Demand','Q_Demand'), ('heat_source_T','heat_source_T'),
+              ('heat_source_T','T_amb'), ('cond_in_T','cond_in_T'))
 
 # Initializing and instantiating a database component:
 db = world.start('DB', step_size=15*60, duration=END)
